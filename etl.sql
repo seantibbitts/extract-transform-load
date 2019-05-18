4b015ffe126e2896e8826366d351cea23e188b0e
S. Tibbitts <47285421+seantibbitts@users.noreply.github.com>
2019-05-18 11:54:16 -0700

    Updated SQL and to_sql

--- etl.sql
+++ etl.sql
@@ -2,34 +2,39 @@ create database gun_db;
 
 use gun_db;
 
+drop table if exists gun_violence;
+drop table if exists rep_gun_donations;
+drop table if exists rep_district;
+
 create table gun_violence (
 	incident_id int primary key,
 	incident_date date,
  	state varchar(50),
+    state_abbr varchar(10),
  	city_or_county varchar(50),
- 	address varchar(255),
+ 	address text,
  	n_killed int,
  	n_injured int,
- 	incident_url varchar(255),
- 	source_url varchar(255),
+ 	incident_url text,
+ 	source_url text,
  	incident_url_fields_missing boolean,
  	congressional_district int,
- 	gun_stolen varchar(255),
- 	gun_type varchar(255),
- 	incident_characteristics varchar(1000),
+ 	gun_stolen text,
+ 	gun_type text,
+ 	incident_characteristics text,
  	latitude decimal(7,4),
- 	location_description varchar(255),
+ 	location_description text,
  	longitude decimal(8,4),
  	n_guns_involved int,
- 	notes varchar(1000),
- 	participant_age varchar(255),
- 	participant_age_group varchar(255),
- 	participant_gender varchar(255),
- 	participant_name varchar(255),
- 	participant_relationship varchar(255),
- 	participant_status varchar(255),
- 	participant_type varchar(255),
- 	sources varchar(1000),
+ 	notes text,
+ 	participant_age text,
+ 	participant_age_group text,
+ 	participant_gender text,
+ 	participant_name text,
+ 	participant_relationship text,
+ 	participant_status text,
+ 	participant_type text,
+ 	sources text,
  	state_house_district int,
  	state_senate_district int
 );
@@ -42,7 +47,7 @@ create table rep_gun_donations (
 	Amount float
 );
 
-create table rep_district (
+create table rep_district (	
 	rep_id int,
 	state_abbr varchar(10),
 	congressional_district int,
